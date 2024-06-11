0347b4014675019fa011c11535776750ad60653a
Ori Afias <ori@afias.dev>
2024-06-11 11:09:50 +0300

    configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+const scope = 7;
+
+const dimensions = new Array(7).fill(null).reduce((acc, _, i) => {
+  acc[i + 1] = `${(i + 1) * 4}rem`;
+  return acc;
+}, {} as any);
+
+const safelist = new Array(scope).fill(null).reduce((acc, _, i) => {
+  acc.push(`h-${i + 1}`);
+  acc.push(`w-${i + 1}`);
+  acc.push(`grid-cols-${i + 1}`);
+  acc.push(`grid-rows-${i + 1}`);
+  acc.push(`row-span-${i + 1}`);
+  acc.push(`col-span-${i + 1}`);
+  return acc;
+}, [] as string[]);
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      height: dimensions,
+      width: dimensions,
     },
   },
+  safelist: safelist,
   plugins: [],
 };
 export default config;

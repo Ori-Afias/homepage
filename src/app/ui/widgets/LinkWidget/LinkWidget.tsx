35e83c0bae1dbdcdd30352968e716f4bf420b0b6
Ori Afias <ori@afias.dev>
2024-06-11 11:11:50 +0300

    link widget

---
+++ src/app/ui/widgets/LinkWidget/LinkWidget.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { Widget, WidgetProps } from '../../primitives/Widget';
+
+interface LinkWidgetProps extends WidgetProps {
+  url: string;
+  image: string;
+}
+
+export function LinkWidget({ url, image, ...rest }: LinkWidgetProps) {
+  return (
+    <Widget {...rest}>
+      <Link href={url} target='_blank'>
+        <div className='h-full w-full bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${image})` }} />
+      </Link>
+    </Widget>
+  );
+}

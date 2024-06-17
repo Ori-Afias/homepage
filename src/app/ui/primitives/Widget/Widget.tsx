2da8ea95b226c3f42fee515a33dcbc354ace8759
Ori Afias <ori@afias.dev>
2024-06-17 11:36:33 +0300

    animate widget

--- src/app/ui/primitives/Widget/Widget.tsx
+++ src/app/ui/primitives/Widget/Widget.tsx
@@ -15,11 +15,11 @@ export function Widget({ className, tiles = [1, 1], title, children }: WidgetPro
   return (
     <div className={`${getDim(tiles)} group`}>
       <section
-        className={`shadow-xl transition-all rounded-2xl overflow-hidden ease-in-out group h-full w-full group-hover:-translate-y-3 group-hover:scale-125 duration-300 ${className}`}
+        className={`group-hover:animate-bounce shadow-xl transition-all rounded-2xl overflow-hidden ease-in-out group h-full w-full duration-300 ${className}`}
       >
         {children}
       </section>
-      <div className='text-xs mt-2 text-center text-white capitalize group-hover:text-sm duration-300 text-ellipsis overflow-hidden'>
+      <div className='text-xs mt-2 text-center text-white capitalize duration-300 text-ellipsis overflow-hidden'>
         {title}
       </div>
     </div>
